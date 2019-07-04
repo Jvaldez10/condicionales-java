62ec78a6895f6b4a060cf1c7d9fdb76a54ffbffb
Jvaldez10 <javaldezh10@gmail.com>
2019-07-04 16:49:53 -0500

    usando if y else en java

---
+++ src/pkg/com/Usando_if_else.java
@@ -0,0 +1,19 @@
+package pkg.com;
+
+public class Usando_if_else {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=45;
+		int b=60;
+		if (a>b) {
+			System.out.println("a NO es mayor que b");
+			
+		} else {
+			System.out.println("b SI es mayor que a");
+		}
+
+	}
+
+}
