273dae5b0b259e5a0607f633948d76815ba16987
Jvaldez10 <javaldezh10@gmail.com>
2019-07-04 16:32:20 -0500

    primer commit

---
+++ src/pkg/com/Usando_if.java
@@ -0,0 +1,16 @@
+package pkg.com;
+
+public class Usando_if {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//if es el condicional mas usado en programadores principciantes
+		
+		int a=3;
+		int b= 7;
+		if (a<b) {
+			System.out.println("el valor de a:["+a+"] es menor que el valor de b ["+b+"]");
+		}
+	}
+
+}
